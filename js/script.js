3b143df414f326601816477420f6760a91248096
Dagmara Gryka <dagagryka@gmail.com>
2021-01-12 15:38:51 +0100

    remove links from sidebar list tags

--- js/script.js
+++ js/script.js
@@ -4,7 +4,9 @@ const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
   optTitleListSelector = '.titles',
   optArticleTagsSelector = '.post-tags .list',
-  optArticleAuthorSelector = '.post-author';
+  optArticleAuthorSelector = '.post-author',
+  optTagsListSelector = '.tags.list';
+
 
 
 
@@ -96,6 +98,9 @@ generateTitleLinks();
 
 function generateTags(){
 
+  /* [NEW] create a new variable allTags with an empty array */
+  let allTags = [];
+
   /* find all articles */
   const articles = document.querySelectorAll('.post');
   console.log(articles);
@@ -129,6 +134,12 @@ function generateTags(){
       /* add generated code to html variable */
       html = html + linkHTML;
 
+      /* [NEW] check if this link is NOT already in allTags */
+      if(allTags.indexOf(linkHTML) == -1){
+        /* [NEW] add generated code to allTags array */
+        allTags.push(linkHTML);
+      }
+
       /* END LOOP: for each tag */
     }
 
@@ -137,6 +148,13 @@ function generateTags(){
 
     /* END LOOP: for every article: */
   }
+
+  /* [NEW] find list of tags in right column */
+  const tagList = document.querySelector('.tags');
+
+  /* [NEW] add html from allTags to tagList */
+  tagList.innerHTML = allTags.join(' ');
+
 }
 
 generateTags();
